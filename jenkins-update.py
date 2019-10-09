027719d9f16df1d3b2afa8d7cc808f10cc9196c3
Joe Ryan <joe@clanryan.us>
2019-10-09 14:02:43 -0400

    updated mock endpoints

--- jenkins-update.py
+++ jenkins-update.py
@@ -2,24 +2,24 @@
 from datetime import datetime as dt
 from os import path
 from xml.etree import ElementTree as ET
+import json
+import logging
+from collections import defaultdict
 
-options = {
-    'DT_FORMAT': '%Y-%m-%d %H:%M:%S.%f',
-    'file_locations': '.'
-} 
+
 # read config or last update
 def read_last_update(filename):
-    print('reading last update datetime')
+    logging.debug('reading last update datetime')
     if path.exists(filename):
         with open(filename) as f:
-           last_update = dt.strptime(f.read().strip(), options['DT_FORMAT'])
+           last_update = dt.strptime(f.read().strip(), options['dt_format'])
         return last_update
     else:
         return dt(1970,1,1,0,0,0,1)
 
 def set_last_update(filename, last_update):
     with open(filename, 'w') as f:
-        f.write(last_update.strftime(options['DT_FORMAT']))
+        f.write(last_update.strftime(options['dt_format']))
 
 # update snapshots and publish
 def update_snapshots(branch):
@@ -32,17 +32,22 @@ def check_if_update_required(branch_repos, last_update):
         print("first snapshot of the day")
         update_required = True
     for repo in branch['repos']:
-        if last_update < check_snapshot_update_time(repo): 
+        if check_snapshot_update_time(repo, last_update): 
             update_required = True
             break
     return update_required
 
-def check_snapshot_update_time(repo):
+def check_snapshot_update_time(repo, last_update):
     print("checking {0}/{1}".format(repo['dist'],repo['branch']))
+    # temp file of xml webapi output
+    for update_time in ET.parse("./mockxml/{}rss.xml".format(repo['branch'])).getiterator('updated'):
+        jenkins_updated = dt.strptime(update_time.text, '%Y-%m-%dT%H:%M:%SZ')
+        print("")
+        if  jenkins_updated > last_update:
+            print("new update: {}".format(update_time.text))
+            return True
+    return False
 
-    # temp mock of json output
-    latest_jenkins_update = dt.strptime(ET.parse("ci-jenkins-{}-{}.xml".format(repo['dist'],repo['branch'])).find('date').text, '%Y-%m-%d %H:%M:%S')
-    return latest_jenkins_update
 
 def rsync_call_to_bash(sync_required):
     if sync_required['stable']:
@@ -50,7 +55,27 @@ def rsync_call_to_bash(sync_required):
     if sync_required['stable'] or sync_required['unstable']:
         print("rsync to CTS ....")
 
+def set_default_options(base_options):
+    if not base_options['dt_format']: base_options['dt_format'] = "%Y-%m-%d %H:%M:%S.%f"
+    # if not base_options['log_level']: base_options['log_level'] = logging.ERROR
+    if not base_options['file_path']: base_options['file_path'] = '.' 
+
+
 if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c','--config', help="configuration file to use", action='store')
+    args = parser.parse_args()
+    if args.config:
+        with open(args.config, 'r') as configfile:
+            options = json.load(configfile)
+    else:
+        options = {
+            'dt_format': "%Y-%m-%d %H:%M:%S.%f",
+            'file_path': '.',
+            'log_level': logging.ERROR
+        }
+    set_default_options(options) 
     current_datetime = dt.now()
     branches = [ {'name': 'stable',
                     'repos': [{'branch': 'fc1', 'dist': 'jessie'},
@@ -64,11 +89,11 @@ if __name__ == '__main__':
     sync_required = {'stable': False, 'unstable': False }
     for branch in branches:
         print("branch name: {}".format(branch['name']))
-        last_update = read_last_update("jenkins-update-{}.date".format(branch['name']))
+        update_file = "{}/jenkins-update-{}.date".format(options['file_path'],branch['name'])
+        last_update = read_last_update(update_file)
         if check_if_update_required(branch, last_update):
             update_snapshots(branch)
             sync_required[branch['name']] = True
-            update_file = "jenkins-update-{}.date".format(branch['name'])
             set_last_update(update_file, current_datetime)
     rsync_call_to_bash(sync_required)
         
